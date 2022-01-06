53213378f36016f52b0ab8decc5f64d6abb78b7a
Hz-Kene <70513168+Hz-Kene@users.noreply.github.com>
2022-01-06 18:42:05 +0100

    Update settings.py

--- hopteens/settings.py
+++ hopteens/settings.py
@@ -68,6 +68,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'hopteens.urls'
 
 TEMPLATES = [
