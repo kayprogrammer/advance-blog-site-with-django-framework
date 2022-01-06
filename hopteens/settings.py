4fb02541489e54990633ceafee63a5be93fa8755
Hz-Kene <70513168+Hz-Kene@users.noreply.github.com>
2022-01-06 06:29:52 +0100

    Update settings.py

--- hopteens/settings.py
+++ hopteens/settings.py
@@ -161,7 +161,7 @@ CKEDITOR_RESTRICT_BY_USER = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
