e14a2154230f94af9002d4f945d2b26930f1e502
Hz-Kene <70513168+Hz-Kene@users.noreply.github.com>
2022-02-24 18:29:53 +0100

    Update settings.py

--- hopteens/settings.py
+++ hopteens/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*ijn)dq3i5k!zprm^^fc9ug(^2&bf5sxatnn$*!&0ot_@ktof@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
